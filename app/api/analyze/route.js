85c7ef6d8b7509b862ffca3ba508e5f61c3f1e93
exyreams <exyreams@gmail.com>
2025-01-16 18:13:18 +0545

    update: varible name

--- app/api/analyze/route.js
+++ app/api/analyze/route.js
@@ -60,8 +60,8 @@ export async function POST(req) {
 
     if (action === "synthesize") {
       const speechConfig = sdk.SpeechConfig.fromSubscription(
-        process.env.AZURE_SPEECH_KEY,
-        process.env.AZURE_SPEECH_REGION,
+        process.env.NEXT_PUBLIC_AZURE_SPEECH_KEY,
+        process.env.NEXT_PUBLIC_AZURE_SPEECH_REGION,
       );
       speechConfig.speechSynthesisLanguage = "en-US";
       speechConfig.speechSynthesisVoiceName =
